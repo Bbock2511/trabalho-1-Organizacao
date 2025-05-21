be161aa026fbe97f908e3161c3d3263285c004d7
Bbock2511 <128552505+Bbock2511@users.noreply.github.com>
2025-05-20 21:14:25 -0300

    Leitura e memória, ler_arquivo_bin é um esqueleto de como usar a leitura

--- codigo/leituraArquivo.asm
+++ codigo/leituraArquivo.asm
@@ -1,44 +1,53 @@
 .data
 #Nome dos arquivos para a leitura
-nomeArquivoBin: .asciiz "trabalho_01-2025_1.bin"
-nomeArquivoDat: .asciiz "trabalho_01-2025_1.dat"
-bufferArquivo: .space 4 
+filenameBin:  .asciiz "arquivo.bin"
+nomeArquivoDat: .asciiz "arquivo.dat"
+mem_text:  .space 4096       # 1KB de espaço para instruções
+bufferArquivo: .space 4
 #############################################
 
 
 .text
 ############Funcoes de Leitura de arquivo###############
 leDat:
-	#abre o arquivo.bin com syscall 13
+	#abre o arquivo .dat com syscall 13
 	li $v0, 13
 	la $a0, nomeArquivoDat
 	li $a1, 0
 	li $a2, 0
 	syscall
-	move $t1, $v0 
+	
+	#$v0 = file descriptor
+	move $t1, $v0
 	
 	#lê o arquivo aberto
 	li $v0, 14
 	move $a0, $t1 #insere o file descriptor como argumento do syscall($a0)
 	la $a1, bufferArquivo #arquivo -> mem_data
-	li $a2, 4
+	li $a2, 361
 	syscall
-	jr $ra
+	
 
 leBin:
-	#abre o arquivo.bin com syscall 13
-	li $v0, 13
-	la $a0, nomeArquivoBin
-	li $a1, 0
-	li $a2, 0
+	# Abrir arquivo
+	li   $v0, 13           # syscall: open
+	la   $a0, filenameBin     # nome do arquivo
+	li   $a1, 0            # modo: leitura
+	li   $a2, 0            # permissão (ignorado aqui)
 	syscall
-	move $t1, $v0 
 	
-	#lê o arquivo aberto
-	li $v0, 14
-	move $a0, $t1
-	la $a1, mem_text #arquivo -> mem_text
-	li $a2, 4
+	move $s0, $v0          # salva o descritor em $s0
+
+	# Ler conteúdo
+	li   $v0, 14           # syscall: read
+	move $a0, $s0          # descritor
+	la   $a1, mem_text     # buffer destino
+	li   $a2, 1024         # número de bytes a ler
 	syscall
-	jr $ra
+
+	# Fechar arquivo
+	li   $v0, 16
+	move $a0, $s0
+	syscall
+
 #####################################################
